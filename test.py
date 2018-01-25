ea310f46b0855a7ab129cd81f36c5d4541fbc821
Tianjian Cai <tjcaiforapp@gmail.com>
2018-01-25 14:59:01 -0500

    output
    finally get the output

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 
 input_size = 5
 output_size = 4
-x = tf.Variable([1,7,4,2,3],tf.float32)
-W = tf.Variable([[1,3,5,7],[2,4,6,8],[3,5,7,9],[4,6,8,2],[5,7,9,3]],tf.float32)
+x = tf.Variable([1.,7,4,2,3],tf.float32)
+W = tf.Variable([[.1,.3,.5,.7],[.2,.4,.6,.8],[.3,.5,.7,.9],[.4,.6,.8,.2],[.5,.7,.9,.3]],tf.float32)
 i = tf.Variable(0)
 sum_z = tf.Variable(tf.zeros([output_size,input_size],tf.float32))
 sum_W = tf.Variable(tf.zeros([output_size,input_size],tf.float32))
@@ -36,8 +36,18 @@ r2,n_sum_W=tf.while_loop(condition, body_W, [i,sum_W])
 f_sum_z = tf.reverse(n_sum_z,[1])
 f_sum_W = tf.reverse(n_sum_W,[1])
 
-print(sess.run(f_sum_z))
-print(sess.run(f_sum_W))
+out_all = tf.divide(f_sum_z, tf.subtract(f_sum_W,1))
+
+out_less = tf.where(tf.less(tf.cast(tf.tile([nx],[output_size,1]),tf.float32),out_all))
+
+out_idx = tf.transpose(tf.concat([[tf.range(0,output_size)],[tf.cast(tf.segment_min(out_less[:, 1], out_less[:, 0]),tf.int32)]],0))
+out = tf.gather_nd(out_all,out_idx)
+
 print(sess.run(nx))
-print(sess.run(nW))
-print(sess.run(nxW))
+#print(sess.run(f_sum_z))
+#print(sess.run(f_sum_W))
+print(sess.run(out_all))
+print(sess.run(out_less))
+print(sess.run(out_idx))
+print(sess.run(out))
+
