81710a89d06b76fc78bdce3434b77bd60a28f4ff
Tianjian Cai <tjcaiforapp@gmail.com>
2018-09-07 14:06:37 -0400

    kitti data handle

--- KITTI_handle.py
+++ KITTI_handle.py
@@ -14,21 +14,35 @@ def chartesian_to_spherical_conversion(xyz):
     rtp[:,2] = np.arctan2(xyz[:,1],xyz[:,0])
     return rtp
 
+
+def rtp_to_xy(rtp):
+    xy = np.zeros_like(rtp)
+    xy[:,0] = (-rtp[:,2]+3.14)*180/3.14
+    xy[:,1] = (rtp[:,1])*180/3.14
+    xy[:,2] = rtp[:,0]
+    return xy
+
+
+def xy_to_map(xy):
+    scale = 2
+    map = 100*np.ones([30*scale,360*scale])
+    for element in xy:
+        if map[int((element[1]-85)*scale-1),int(element[0]*scale-1)] > element[2]:
+            map[int((element[1]-85)*scale-1),int(element[0]*scale-1)] = element[2]
+    return map
+
 path = os.listdir(os.getcwd()+"/KITTI_data/2011_09_26/2011_09_26_drive_0005_sync/velodyne_points/data/")
 print(path)
-scan = np.fromfile(os.getcwd()+"/KITTI_data/2011_09_26/2011_09_26_drive_0005_sync/velodyne_points/data/" + path[120], dtype=np.float32)
+scan = np.fromfile(os.getcwd()+"/KITTI_data/2011_09_26/2011_09_26_drive_0005_sync/velodyne_points/data/" + path[30], dtype=np.float32)
 scan = np.reshape(scan,[-1,4])
 rtp = chartesian_to_spherical_conversion(scan)
 
-f1 = plt.figure(figsize=(20,4))
-ax1 = f1.add_subplot(111)
-# Plot every 100th point so things don't get too bogged down
-velo_range2 = range(0, rtp.shape[0], 1)
-ax1.scatter(-rtp[velo_range2, 2],
-            -rtp[velo_range2, 1],
-            c=rtp[velo_range2, 0],
-            s=1)
+xy = rtp_to_xy(rtp)
+map = xy_to_map(xy)
 
+f2 = plt.figure(figsize=(20,4))
+ax2 = f2.add_subplot(111)
+ax2.imshow(map)
 
 
 plt.show()
