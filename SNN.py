2b993b72b9500cb05944d6f39e17c5dc5a3f6464
Tianjian Cai <tjcaiforapp@gmail.com>
2018-09-07 14:57:57 -0400

    Linear regression seems work?

--- SNN.py
+++ SNN.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 
 line = np.linspace(0,10,100)
-groundtruth = 5*line + 15
-noise = np.random.normal(0,5,100)
+groundtruth = 15*line + 21
+noise = np.random.normal(0,20,100)
 noised_output = groundtruth+noise
 '''
 f1 = plt.subplot(211)
@@ -16,21 +16,39 @@ plt.show()
 '''
 
 
-k = torch.tensor(0.,dtype=torch.float32,requires_grad=True)
-b = torch.tensor(0.,dtype=torch.float32,requires_grad=True)
+k = torch.randn(1,dtype=torch.float32,requires_grad=True)
+b = torch.randn(1,dtype=torch.float32,requires_grad=True)
 l = torch.tensor(line,dtype=torch.float32)
 
-output = k * l + b.expand(l.size())
+lr = 1e-2
 
-loss = torch.mean((output - torch.tensor(noised_output,dtype=torch.float32))**2)
+i = 0
+old_loss = 0
+while True:
+    output = k.expand(l.size()) * l + b.expand(l.size())
+    loss = torch.mean((output - torch.tensor(noised_output, dtype=torch.float32)) ** 2)
+    loss.backward()
+    print(repr(i) + " loss: "+repr(loss.item()))
+    if abs(loss.item() - old_loss) < 1e-6:
+        break
+    else:
+        old_loss = loss.item()
+    with torch.no_grad():
+        k -= lr * k.grad
+        b -= lr * b.grad
+        k.grad.zero_()
+        b.grad.zero_()
+    i = i + 1
 
-lr = 1e-3
-for i in range(1000):
-    print("this is loss: "+repr(loss))
-    loss.backward(retain_graph=True)
-    k = k - k.grad
-    b = b - b.grad
+output = k.expand(l.size()) * l + b.expand(l.size())
+o = output.data.tolist()
+print(o)
 
+print(k.item(),b.item())
+
+plt.plot(line,o)
+plt.scatter(line,noised_output)
+plt.show()
 
 
 
