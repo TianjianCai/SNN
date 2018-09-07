6460361fc907014bfbf31d6bcb3bc04c89f195f5
Tianjian Cai <tjcaiforapp@gmail.com>
2018-09-07 17:05:52 -0400

    use pytorch module

--- SNN.py
+++ SNN.py
@@ -1,53 +1,58 @@
 import numpy as np
 import torch
+import torch.nn as nn
 import matplotlib.pyplot as plt
 
 
-line = np.linspace(0,10,100)
-groundtruth = 15*line + 21
-noise = np.random.normal(0,20,100)
+line = np.linspace(0,10,1000)
+groundtruth = -15*line + 49
+noise = np.random.normal(0,21,1000)
 noised_output = groundtruth+noise
-'''
-f1 = plt.subplot(211)
-f1.scatter(line,groundtruth)
-f2 = plt.subplot(212)
-f2.scatter(line,noised_output)
-plt.show()
-'''
 
-
-k = torch.randn(1,dtype=torch.float32,requires_grad=True)
-b = torch.randn(1,dtype=torch.float32,requires_grad=True)
-l = torch.tensor(line,dtype=torch.float32)
 
 lr = 1e-3
+
+
+class layer(nn.Module):
+    def __init__(self):
+        super(layer, self).__init__()
+        self.k = nn.Parameter(torch.zeros(1))
+        self.b = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        return self.k.expand(x.size()) * x + self.b.expand(x.size())
+
+
+module = layer()
 
 i = 0
 old_loss = 0
 while True:
-    output = k.expand(l.size()) * l + b.expand(l.size())
+    l = torch.tensor(line,dtype=torch.float32)
+    output = module(l)
     loss = torch.mean((output - torch.tensor(noised_output, dtype=torch.float32)) ** 2)
     loss.backward()
     print(repr(i) + " loss: "+repr(loss.item()))
-    if abs(loss.item() - old_loss) < 1e-6:
+    if abs(loss.item() - old_loss) < 1e-5:
         break
     else:
         old_loss = loss.item()
     with torch.no_grad():
-        k -= lr * k.grad
-        b -= lr * b.grad
-        k.grad.zero_()
-        b.grad.zero_()
+        module.k -= lr * module.k.grad
+        module.b -= lr * module.b.grad
+        module.k.grad.zero_()
+        module.b.grad.zero_()
     i = i + 1
 
-output = k.expand(l.size()) * l + b.expand(l.size())
+output = module(l)
 o = output.data.tolist()
 print(o)
 
-print(k.item(),b.item())
+print(module.k.item(),module.b.item())
 
-plt.plot(line,o)
-plt.scatter(line,noised_output)
+plt.scatter(line,noised_output,s=0.5)
+plt.plot(line,o,color='r')
+
 plt.show()
 
 
